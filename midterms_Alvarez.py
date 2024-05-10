eec9423e06d7803a7732dd01fb8c72dcf1324f70
francineannealvarez <23-09780@g.batstate-u.edu.ph>
2024-05-10 11:10:56 +0800

    game_library

--- midterms_Alvarez.py
+++ midterms_Alvarez.py
@@ -1,4 +1,7 @@
-#dictionary
+#dictionary to store game library with their quantities and rental costs
+#used numbers to lessen errors in typing names of games
 game_library = {
-    1: {}
-}
+        1: {"Donkey Kong": {"quantity": 3, "rental cost": 2}},
+        2: {"Super Mario Bros": {"quantity": 5, "rental cost": 3}},
+        3: {"Tetris": {"quantity": 2, "rental cost": 1}}
+        }
