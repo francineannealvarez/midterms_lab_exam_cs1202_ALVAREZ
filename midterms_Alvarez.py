60af613f90875f6d94ecf212d938261ef5fdc6d7
francineannealvarez <23-09780@g.batstate-u.edu.ph>
2024-05-10 11:23:55 +0800

    functions

--- midterms_Alvarez.py
+++ midterms_Alvarez.py
@@ -13,3 +13,60 @@ user_accounts = {}
 admin_username = "admin"
 admin_password = "adminpass"
 
+#Function to display available games with their numbers and rental costs
+def display_available_games():
+    pass
+
+#Function to register a new user
+def register_user():
+    pass
+#Function to rent a game 
+def rent_game(username):
+    pass
+
+#Function to return a game
+def return_game(username):
+    pass
+
+#Function to top_up user account
+def top_up_account(username):
+    pass
+
+#Function to display user's inventory
+def display_inventory(username):
+    pass
+
+#Function for admin to update game details
+def admin_update_game(username):
+    pass
+
+#Function for admin log in
+def admin_login():
+    pass
+
+#Function for admin menu
+def admin_menu():
+    pass
+
+#Function for users to redeem points for a free game rental
+def redeem_free_rental(username):
+    pass
+
+#Function to display game inventory
+def display_game_inventory():
+    pass
+
+#Function to handle user's logged in menu
+def logged_in_menu(username):
+    pass
+
+#Function to check user credentials
+def check_credentials(username, password):
+    pass
+
+#Main function to run the program
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
